7888a47f18ce6de1e55975fc40ee5bb0b9d1c01b
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-01 18:00:17 +0100

    algo minimum convert

--- exercices/exo1.php
+++ exercices/exo1.php
@@ -1,28 +1,52 @@
 <?php
 
-function fibonacci($nbr){
+// function fibonacci($nbr){
 
-    if($nbr <= 1){
-        return $nbr;
+//     if($nbr <= 1){
+//         return $nbr;
+//     }
+
+//     $a = 0;
+//     $b = 1;
+
+//     for($i = 0 ; $i < $nbr; $i++){
+
+//         $res = $a + $b;
+//         $a = $b;
+//         $b = $res;
+//     }
+
+//     return $b;
+
+
+// }
+
+// echo fibonacci(1);
+// echo fibonacci(2);
+// echo fibonacci(3);
+// echo fibonacci(4);
+// echo fibonacci(5);
+
+$coins = [5, 7, 1, 1, 2, 3, 22];
+
+sort($coins);
+
+function minimumConvert($arr){
+
+    $minc = 1;
+
+    foreach($arr as $value){
+
+        if($value <= $minc){
+            $minc = $value + $minc;
+        }
+
+
     }
 
-    $a = 0;
-    $b = 1;
-
-    for($i = 0 ; $i < $nbr; $i++){
-
-        $res = $a + $b;
-        $a = $b;
-        $b = $res;
-    }
-
-    return $b;
+    return $minc;
 
 
 }
-
-echo fibonacci(1);
-echo fibonacci(2);
-echo fibonacci(3);
-echo fibonacci(4);
-echo fibonacci(5);
+echo minimumConvert($coins);
+die();
