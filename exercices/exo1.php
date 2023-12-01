74212da63b40dcc4aef3cbe6edd7bbf42b7a9b5e
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-01 18:02:35 +0100

    algo récursif fisive fibo

--- exercices/exo1.php
+++ exercices/exo1.php
@@ -27,26 +27,40 @@
 // echo fibonacci(4);
 // echo fibonacci(5);
 
-$coins = [5, 7, 1, 1, 2, 3, 22];
+// $coins = [5, 7, 1, 1, 2, 3, 22];
 
-sort($coins);
+// sort($coins);
 
-function minimumConvert($arr){
+// function minimumConvert($arr){
 
-    $minc = 1;
+//     $minc = 1;
 
-    foreach($arr as $value){
+//     foreach($arr as $value){
 
-        if($value <= $minc){
-            $minc = $value + $minc;
-        }
+//         if($value <= $minc){
+//             $minc = $value + $minc;
+//         }
 
 
+//     }
+
+//     return $minc;
+
+
+// }
+// echo minimumConvert($coins);
+// die();
+
+function fib($nbr){
+    if($nbr <= 1){
+        return $nbr;
     }
-
-    return $minc;
-
-
+    return fib($nbr - 1) + fib($nbr - 2);
 }
-echo minimumConvert($coins);
-die();
+
+echo fib(1);
+echo fib(2);
+echo fib(3);
+echo fib(4);
+echo fib(5);
+echo fib(6);
